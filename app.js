f1aaf2497e5d9cb8c465c7e3eecbcfeaba036eac
Kinsey Ashby <kinseyzaire@gmail.com>
2015-12-10 23:29:06 -0700

    got the keyboard controls to work

--- app.js
+++ app.js
@@ -1,37 +1,74 @@
 $(document).ready(function(){
    $("#logo").animate({"opacity":1.0},1500,function(){console.log()});
    $("#quilt").animate({"opacity":1.0},2000,function(){console.log()});
-})
+});
 
+var theGlow = $(".glow");
 $("#clickFerPieces").click(function(){
    var colorChoice = $("input[name=color]:checked").val();
    var qty = $("#qty").val();
+   var layEmOut = 35;
    for (var i = 0; i < qty; i++) {
-      $("#quilt").append("<img class='piece diamond' src='images/colors/"+ colorChoice +"/"+ i +".png' style='z-index:"+ i +"'>");
-   };
+      $("#quilt").append("<img class='piece diamond' src='images/colors/"+ colorChoice +"/"+ i +".png' style='z-index: "+ i +"; left: "+(layEmOut+(i*15))+"px;'>");
+      };
+});
+
+$(document).on('click', theGlow, function(){
    $(".piece").draggable({
       start: function(event,ui){
-         console.log("started");
          $(this).addClass("glow");
       },
       stop: function(event,ui){
-         console.log("I have stopped");
          $(this).removeClass("glow");
       }
    });
 });
 
-
-
-$(document).on('keypress','.glow', function(e){
-   console.log("pushed a key while holding a thing");
+$(document).on('keypress', theGlow, function(e){
    switch (e.keyCode) {
-      case 82: mythis.rotate(60); break;
-      case 114: mythis.rotate(60); break;
-      case 67: mythis.clone().appendTo("#quilt"); break;
-      case 99: mythis.clone().appendTo("#quilt"); break;
-      case 68: mythis.hide(); break;
-      case 100: mythis.hide(); break;
+      case 82:; rotate60(); break;
+      case 114: rotate60(); break;
+      case 67: cccopy(); break;
+      case 99: cccopy(); break;
+      case 68: $(".glow").hide(); break;
+      case 100: $(".glow").hide(); break;
       default: return true;
    };
 });
+
+
+var degreesCount = 0;
+var rotate45 = function() {
+   if (degreesCount >= 360) {
+      degreesCount = 45;
+      return $(".glow").rotate(degreesCount);
+   } else {
+      degreesCount += 45;
+      return $(".glow").rotate(degreesCount);
+   }
+};
+var rotate60 = function() {
+   if (degreesCount >= 360) {
+      degreesCount = 60;
+      return $(".glow").rotate(degreesCount);
+   } else {
+      degreesCount += 60;
+      return $(".glow").rotate(degreesCount);
+   }
+};
+var rotate90 = function() {
+   if (degreesCount >= 360) {
+      degreesCount = 90;
+      return $(".glow").rotate(degreesCount);
+   } else {
+      degreesCount += 90;
+      return $(".glow").rotate(degreesCount);
+   }
+};
+
+var cccopy = function(){
+   var copyDuhPiece = $(".glow")[0].outerHTML;
+   var imageBuziness = copyDuhPiece.replace(' ui-draggable ui-draggable-handle ui-draggable-dragging glow','');
+   copyDuhPiece = '"'+ imageBuziness + '"';
+   $("#quilt").append(copyDuhPiece);
+};
